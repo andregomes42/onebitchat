0365f884bf6aa22019e9e7694f3e159b67cb39b4
costelinha <andregomesngr@gmail.com>
2020-09-09 11:42:56 -0500

    inlcude models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_204643) do
+ActiveRecord::Schema.define(version: 2020_09_09_154947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.string "slug"
+    t.bigint "user_id"
+    t.bigint "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_channels_on_team_id"
+    t.index ["user_id"], name: "index_channels_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.integer "messagable_id"
+    t.string "messagable_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "talks", force: :cascade do |t|
+    t.integer "user_one_id"
+    t.integer "user_two_id"
+    t.bigint "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_talks_on_team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "slug"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teams_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -28,4 +65,9 @@ ActiveRecord::Schema.define(version: 2020_09_08_204643) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "channels", "teams"
+  add_foreign_key "channels", "users"
+  add_foreign_key "messages", "users"
+  add_foreign_key "talks", "teams"
+  add_foreign_key "teams", "users"
 end
